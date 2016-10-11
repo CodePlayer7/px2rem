70bf745d51d27db03a712b4675524b17600cd1fe
callmexyz <amos.callmexyz@gmail.com>
2016-10-11 23:32:50 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var _ = require('lodash');
+var utils = require('loader-utils');
+
+
+module.exports = function (source) {
+    if(_.isUndefined(this.cacheable)) return source;
+    this.cacheable();
+    var query = utils.parseQuery(this.query),
+        basePx = !_.isUndefined(query.basePx)?query.basePx:10,
+        min =!_.isUndefined(query.min)?query.min:0,
+        floatWidth =!_.isUndefined(query.floatWidth)?query.floatWidth:3,
+        matchPXExp = /\b\d+(\.\d+)?px\b/g;
+    return source.replace(matchPXExp, function (match) {
+        var pxValue= parseFloat(match.slice(0,match.length-2));
+        if(pxValue<=min) return match;
+        var remValue = pxValue/basePx;
+        if(remValue%1==0) return remValue;
+        return  (pxValue/basePx).toFixed(floatWidth)+'rem';
+    });
+
+};
+
