28d5ffed14b50f69a4d534451f0afb987281e4bd
Wang Jun <amos.callmexyz@gmail.com>
2016-10-12 10:05:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-
 var _ = require('lodash');
 var utils = require('loader-utils');
 
 
 module.exports = function (source) {
-    if(_.isUndefined(this.cacheable)) return source;
+    if (_.isUndefined(this.cacheable)) return source;
     this.cacheable();
     var query = utils.parseQuery(this.query),
-        basePx = !_.isUndefined(query.basePx)?query.basePx:10,
-        min =!_.isUndefined(query.min)?query.min:0,
-        floatWidth =!_.isUndefined(query.floatWidth)?query.floatWidth:3,
+        basePx = !_.isUndefined(query.basePx) ? query.basePx : 10,
+        min = !_.isUndefined(query.min) ? query.min : 0,
+        floatWidth = !_.isUndefined(query.floatWidth) ? query.floatWidth : 3,
         matchPXExp = /\b\d+(\.\d+)?px\b/g;
     return source.replace(matchPXExp, function (match) {
-        var pxValue= parseFloat(match.slice(0,match.length-2));
-        if(pxValue<=min) return match;
-        var remValue = pxValue/basePx;
-        if(remValue%1==0) return remValue;
-        return  (pxValue/basePx).toFixed(floatWidth)+'rem';
+        var pxValue = parseFloat(match.slice(0, match.length - 2));
+        if (pxValue <= min) return match;
+        var remValue = pxValue / basePx;
+        if (pxValue % basePx != 0)
+            remValue = (pxValue / basePx).toFixed(floatWidth);
+        return remValue + 'rem';
     });
 
 };
